c8baaa876c549ba2ff4fcd44fdb3dc3a9fed2c60
AnhQuanTrl <anhquan.tranle0601@gmail.com>
2024-04-22 10:24:29 +0700

    feat: change python to basedpyright

--- home/private_dot_config/nvim/lua/arthur/pde/python.lua
+++ home/private_dot_config/nvim/lua/arthur/pde/python.lua
@@ -1,3 +1,5 @@
+local Util = require 'arthur.util'
+
 return {
   {
     'nvim-treesitter/nvim-treesitter',
@@ -9,22 +11,41 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        pylsp = {
+        basedpyright = {
           mason = false,
           settings = {
-            pylsp = {
-              plugins = {
-                ruff = {
-                  enabled = true, -- Enable the plugin
-                  formatEnabled = true, --
-                },
-                -- rope_autoimport = {
-                --   enabled = true,
-                -- },
-              },
-            },
+            basedpyright = {},
           },
         },
+        ruff_lsp = {
+          mason = false,
+          keys = {
+            {
+              '<leader>co',
+              function()
+                vim.lsp.buf.code_action {
+                  apply = true,
+                  context = {
+                    only = { 'source.organizeImports' },
+                    diagnostics = {},
+                  },
+                }
+              end,
+              desc = 'Organize Imports',
+            },
+          },
+          settings = {},
+        },
+      },
+      setup = {
+        ruff_lsp = function()
+          Util.lsp.on_attach(function(client, _)
+            if client.name == 'ruff_lsp' then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
       },
     },
   },
